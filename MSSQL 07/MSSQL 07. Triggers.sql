506a366afceea914cdc4ebfe62ecb9f0804c4f45
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-02-17 09:17:05 +0400

    Triggers

---
+++ MSSQL 07/MSSQL 07. Triggers.sql
@@ -0,0 +1,91 @@
+﻿-- Triggers
+-- DML Triigers
+-- DML Events (INSERT, UPDATE, DELETE)
+
+USE [Library]
+
+--CREATE TRIGGER BookUpdates
+--ON Books
+--AFTER UPDATE
+--AS
+--BEGIN
+--	PRINT @@ROWCOUNT
+--END
+
+--UPDATE Books
+--SET Books.Pages += 1
+
+--UPDATE Books
+--SET Books.Pages += 1
+--WHERE Books.Pages > 500
+
+--CREATE TRIGGER OldBooks
+--ON Books
+--AFTER INSERT
+--AS
+--BEGIN
+--	DECLARE @year int
+--	SELECT @year = YearPress
+--	FROM inserted
+
+--	IF @year < 2000
+--		BEGIN
+--			PRINT N'Biz belə Nadir müəllim kimi köhnə kitabları əlavə edə bilmərik'
+--			ROLLBACK TRAN
+--		END
+--	ELSE PRINT N'Əla. Belə təzə kitablar gətirin deyək hə'
+--END
+
+--INSERT INTO Books (Id, [Name], Pages, 
+--					YearPress, Id_Themes, Id_Category,
+--					Id_Author, Id_Press, Quantity)
+--VALUES (113, 'Imtahana az Qalıb', 1254, 2025, 2, 1, 1, 5, 11)
+
+--INSERT INTO Books (Id, [Name], Pages, 
+--					YearPress, Id_Themes, Id_Category,
+--					Id_Author, Id_Press, Quantity)
+--VALUES (114, 'Elvin müəllim, dərsə qulaq asın', 1254, 1999, 2, 1, 1, 5, 11)
+
+--CREATE TRIGGER NotPiter
+--ON Books
+--AFTER DELETE
+--AS
+--	BEGIN
+
+--		IF EXISTS(
+--			SELECT *
+--			FROM deleted
+--			WHERE Id_Press = 3
+--		)
+--		BEGIN
+--			PRINT N'Piterə əl dəymə yuxardan tapşırıblar'
+--			ROLLBACK TRAN
+--		END
+--	END
+
+--DELETE 
+--FROM Books
+--WHERE Id = 113
+
+CREATE TRIGGER NoInsertLibrarian
+ON Libs
+INSTEAD OF INSERT
+AS
+BEGIN
+	PRINT 'THIS IS SPARTAAAAA!'
+END
+
+INSERT INTO Libs(Id, FirstName, LastName)
+VALUES (3, 'ELon', 'Əliyev')
+
+--DECLARE @i AS int = 0
+--WHILE @i < 1000000
+--BEGIN
+--		INSERT INTO Libs(Id, FirstName, LastName)
+--		VALUES (3, 'ELon', 'Əliyev')
+
+--		SET @i+=1
+--END
+
+
+DROP TRIGGER NoInsertLibrarian
---
+++ MSSQL 07/MSSQL 07. UDF.sql
@@ -0,0 +1,86 @@
+﻿-- User Definition Functions (UDF)
+
+-- UDF iki tipdə olur
+-- Scalar-valued functions
+-- Table-valued functions
+
+Use [Library]
+
+
+GO
+
+-- scalar-valued functions
+
+--CREATE FUNCTION CountBooksForReturn
+--(@stud_id AS int)
+--RETURNS int
+--AS
+--BEGIN
+--	DECLARE @books AS int = 0
+--	SELECT @books = COUNT(*)
+--	FROM S_Cards
+--	WHERE S_Cards.Id_Student = @stud_id AND S_Cards.DateIn IS NULL
+
+--	RETURN @books
+--END
+
+--SELECT dbo.CountBooksForReturn(5) AS BooksCount
+
+--DECLARE @books_count AS int
+--EXEC @books_count = CountBooksForReturn 5
+--SELECT @books_count AS BooksCount
+
+
+-- Table-valued functions
+--CREATE FUNCTION BookList()
+--RETURNS TABLE
+--AS
+--RETURN (
+--	SELECT B.[Name] AS BookName, C.[Name] AS Category,
+--				T.[Name] AS Themes, 
+--				A.FirstName + ' ' + A.LastName AS Author 
+--	FROM Books AS B
+--	INNER JOIN Authors AS A ON B.Id_Author = A.Id
+--	INNER JOIN Categories AS C ON B.Id_Category = C.Id
+--	INNER JOIN Themes AS T ON B.Id_Themes = T.Id
+--)
+
+--SELECT *
+--FROM BookList() 
+
+--CREATE FUNCTION BooksCountByAuthors()
+--RETURNS @BooksAuthors TABLE (Author nvarchar(30), BooksCount int)
+--AS
+--BEGIN
+--	DECLARE @tmp_books TABLE (Author nvarchar(30), BooksCount int)
+--	INSERT @tmp_books
+	--SELECT A.FirstName + ' ' + A.LastName AS Author, COUNT(SC.Id_Book)
+	--FROM Authors AS A 
+	--INNER JOIN Books AS B ON A.Id = B.Id_Author
+	--INNER JOIN S_Cards AS SC ON SC.Id_Book = B.Id
+	--GROUP BY A.FirstName, A.LastName
+
+	----INSERT @tmp_books
+	--SELECT A.FirstName + ' ' + A.LastName AS Author, COUNT(TC.Id_Book)
+	--FROM Authors AS A 
+	--INNER JOIN Books AS B ON A.Id = B.Id_Author
+	--INNER JOIN T_Cards AS TC ON TC.Id_Book = B.Id
+	--GROUP BY A.FirstName, A.LastName
+
+--	DECLARE @tmp_books2 TABLE (Author nvarchar(30), BooksCount int)
+--	INSERT @tmp_books2
+--	SELECT T.Author, SUM(T.BooksCount)
+--	FROM @tmp_books AS T
+--	GROUP BY T.Author
+
+--	INSERT @BooksAuthors
+--	SELECT T.Author, T.BooksCount
+--	FROM @tmp_books2 AS T
+
+--	RETURN
+--END
+
+
+--SELECT TOP(1) Author, BooksCount
+--FROM BooksCountByAuthors() AS BC
+--ORDER BY  BooksCount DESC
